222c5c947344dc0c635a93286bc98aaca505bc56
vinaycharlie01 <106230026+vinaycharlie01@users.noreply.github.com>
2022-06-16 15:07:15 +0530

    Create main.go

---
+++ golearn/gobasic/fmtpkg/main.go
@@ -0,0 +1,41 @@
+package main
+
+func main() {
+	// //Coding Exercise #1
+
+	// //Consider the following variable declarations:
+
+	// x, y, z := 10, 15.5, "Gophers"
+	// score := []int{10, 20, 30}
+	// //Using fmt.Printf():
+
+	// //Print each variable using a specific verb for its type
+	// fmt.Printf("x is %d, y is %f, z is %s\n", x, y, z)
+	// fmt.Printf("score is %#v\n", score)
+	// //Print the string value enclosed by double quotes ("Gophers")
+	// fmt.Printf("z is %q\n", z)
+	// //Print each variable using the same verb
+	// fmt.Printf("x is %v, y is %v, z is %v, score is %v\n", x, y, z, score)
+	// //Print the type of y and score
+	// fmt.Printf("y type: %T, score type: %T\n", y, score)
+
+	// //Coding Exercise #2
+
+	// //Consider the following constant declaration:
+	// const x float64 = 1.422349587101
+
+	// //Write a Go program that prints the value of x with 4 decimal points.
+	// fmt.Printf("x is %.4f\n", x)
+
+	// //Coding Exercise #3
+
+	// //There are some errors in the following Go program. Try to identify the errors, change the code and run the program without errors.
+	// shape := "circle"
+	// radius := 3.2
+	// const pi float64 = 3.14159
+	// circumference := 2 * pi * radius
+
+	// fmt.Printf("Shape: %q\n", shape)
+	// fmt.Printf("Circle's circumference with radius %d is %b\n", int(radius), int(circumference))
+	// _ = shape
+}
