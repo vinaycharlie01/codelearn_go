0e6098a452401b5a49e287da8d745d3528552414
vinaycharlie01 <106230026+vinaycharlie01@users.noreply.github.com>
2022-06-16 16:11:14 +0530

    Create main.go

---
+++ golearn/flowcontrol/main.go
@@ -0,0 +1,80 @@
+package main
+
+func main() {
+	// //Coding Exercise #1
+	// //Using a for loop and an if statement print out all the numbers between 1 and 50 that divisible by 7.
+	// for i := 1; i <= 50; i++ {
+	// 	if i%7 == 0 {
+	// 		fmt.Println(i)
+
+	// 	}
+	// }
+
+	// //Coding Exercise #2
+	// //Change the code from the previous exercise and use the continue statement to print out all the numbers divisible by 7 between 1 and 50.
+	// for i := 1; i <= 50; i++ {
+	// 	if i%7 == 0 {
+	// 		continue
+	// 	}
+	// 	fmt.Println(i)
+	// }
+
+	// //Coding Exercise #3
+	// //Change the code from the previous exercise and use the break statement to print out only the first 3 numbers divisible by 7 between 1 and 50.
+
+	// count := 0
+	// for i := 1; i <= 50; i++ {
+	// 	if i%7 == 0 {
+	// 		fmt.Println(i)
+	// 		count += 1
+	// 		if count == 3 {
+	// 			break
+	// 		}
+	// 	}
+	// }
+
+	// //Coding Exercise #4
+	// //Using a for loop, an if statement and the logical and operator print out all the numbers between 1 and 500 that divisible both by 7 and 5.
+
+	// for i := 1; i <= 500; i++ {
+	// 	if i%7 == 0 && i%5 == 0 {
+	// 		fmt.Println(i)
+	// 	}
+
+	// }
+
+	// //Coding Exercise #5
+	// //Using a for loop print out all the years from your birthday to the current year.
+	// //Use a variant of for loop where the post statement is moved inside the for block of code.
+	// count := 0
+	// for i := 2000; i < 2022; {
+	// 	count += 1
+	// 	fmt.Println("tgis is year", i)
+	// 	i++
+
+	// }
+	// fmt.Println(count)
+
+	// //Coding Exercise #6
+	// //Consider the following Go
+	// age := -9
+	// switch {
+	// case age < 0 || age > 100:
+	// 	fmt.Println("Invalid Age")
+	// case age < 18:
+	// 	fmt.Println("You are minor!")
+	// case age == 18:
+	// 	fmt.Println("Congratulations! You've just become major!")
+	// default:
+	// 	fmt.Println("You are major!")
+	// }
+
+	// b := 10
+	// switch {
+	// case b <= 20:
+	// 	fmt.Println("tjdkieen 20")
+	// case b <= 30:
+	// 	fmt.Println("rtjmfri 30")
+
+	// }
+}
