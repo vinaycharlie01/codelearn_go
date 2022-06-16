385f294a26ad39e9a67f70b8b8769bbeec9e309f
vinaycharlie01 <106230026+vinaycharlie01@users.noreply.github.com>
2022-06-16 14:52:28 +0530

    Create main.go

---
+++ golearn/gobasic/constant/main.go
@@ -0,0 +1,83 @@
+package main
+
+func main() {
+	// //Coding Exercise #1
+
+	// //Using the const keyword declare and initialize the following constants:
+
+	// //1. daysWeek with value 7
+	// const daysWeek = 7
+
+	// //2. lightSpeed with value 299792458
+	// const lightSpeed = 299792458
+
+	// //3. pi with value 3.14159
+	// const pi = 3.14159
+
+	// //Run the program without errors.
+	// _, _, _ = daysWeek, lightSpeed, pi
+
+	// //Coding Exercise #2
+
+	// //Change the code from the previous exercise and declare all 3 constants as grouped constants.
+	// const (
+	// 	daysWeek   = 7
+	// 	lightSpeed = 299792458
+	// 	pi         = 3.14159
+	// )
+	// _, _, _ = daysWeek, lightSpeed, pi
+	// //Make them untyped.
+
+	// //Coding Exercise #3
+	// //Calculate how many seconds are in a year.
+
+	// //STEPS:
+
+	// //1. Declare secPerDay constant and initialize it to the number of seconds in a day
+	// const secPerDay = 60 * 60 * 24
+	// //2. Declare daysYear constant and initialize it to 365
+	// const daysYear = 365
+	// //3. Use fmt.Printf() to print out the total number of seconds in a year.
+	// fmt.Printf("%d", secPerDay*daysYear)
+	// //EXPECTED OUTPUT:
+
+	// // There are 31536000 seconds in a year.
+
+	// //Coding Exercise #4
+
+	// //There are an error in the following Go program. Try to identify the error, change the code and run the program without errors.
+
+	// const x int = 10
+
+	// // declaring a constant of type slice int ([]int)
+	// // const m = []int{1: 3, 4: 5, 6: 8}
+	// _ = x
+
+	// //Coding Exercise #5
+
+	// //There are some errors in the following Go program. Try to identify the errors, change the code and run the program without errors.
+	// const a float64 = 7.3
+	// const b = 5
+	// const c = a * b
+	// _ = c
+	// fmt.Println(c)
+
+	// x := 8
+	// _ = x
+
+	// var noIPv6 = math.Pow(b, 2.)
+	// fmt.Println(noIPv6)
+
+	// //Coding Exercise #6
+
+	// //Using Iota declare the following months of the year: Jun, Jul and Aug
+	// const (
+	// 	jun = 6 + iota
+	// 	Jul
+	// 	aug
+	// )
+	// //Jun, Jul and Aug are constant and their value is 6, 7 and 8.
+	// fmt.Println(jun, Jul, aug)
+
+	
+}
