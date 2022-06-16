abf25d945181efb8b511fcd91dde2c25aa7de0ff
vinaycharlie01 <106230026+vinaycharlie01@users.noreply.github.com>
2022-06-16 15:28:50 +0530

    Create main.go

---
+++ golearn/gobasic/operators&conv/main.go
@@ -0,0 +1,76 @@
+package main
+
+func main() {
+	// var i = 3
+	// var f = 3.2
+	// var s1, s2 = "3.14", "5"
+
+	// // 1. int to string
+	// s := strconv.Itoa(i)
+	// fmt.Printf("s Type is %T, s value is %q\n", s, s)
+
+	// // 2. string to int
+	// is, err := strconv.Atoi(s2)
+	// if err == nil {
+	// 	fmt.Printf("i type is %T, i value is %v\n", is, is)
+	// } else {
+	// 	fmt.Println("Can not convert string to int.")
+	// }
+
+	// // 3. float64 to string
+	// ss1 := fmt.Sprintf("%f", f)
+	// fmt.Printf("ss1's type: %T, ss1's value: %s\n", ss1, ss1)
+
+	// // 4. string to float64
+	// f1, err1 := strconv.ParseFloat(s1, 64)
+	// if err1 == nil {
+	// 	fmt.Printf("f1's type: %T, f1's value: %v\n", f1, f1)
+	// } else {
+	// 	fmt.Println("Cannot convert string to float64.")
+	// }
+
+	// //Coding Exercise #3
+
+	// //There are some errors in the following Go program. Try to identify the errors, change the code and run the program without errors.
+	// x, y := 4, 5.1
+
+	// z := x * int(y)
+	// fmt.Println(z)
+
+	// a := 5
+	// b := 6.2 * float64(a)
+	// fmt.Println(b)
+
+	// 	//Coding Exercise #4
+
+	// //Create a Go program that computes how long does it take for the Sunlight to reach the Earth if we know that the distance from the Sun to Earth is 149.6 million km and the speed of light  is 299,792,458 m / s
+
+	// 	var i = 3
+	// 	var f = 3.2
+
+	// 	// int to float64
+	// 	f1 := float64(i)
+	// 	fmt.Printf("f1's type: %T, f1's value: %f\n", f1, f1)
+
+	// 	// float64 to int
+
+	// 	i1 := int(f)
+	// 	fmt.Printf("i1's type: %T, i1's value: %d\n", i1, i1)
+
+	// //Coding Exercise #5
+
+	// //Write the correct logical operator (&&, || , !) inside the expression so that result1 will be false and result2 will be true.
+
+	// x, y := 0.1, 5
+	// var z float64
+
+	// // Write the correct logical operator (&&, || , !)
+	// // inside the expression so that result1 will be false and result2 will be true.
+
+	// result1 := x < z || int(x) != int(z)
+	// fmt.Println(result1)
+
+	// result2 := y == 1*5 && int(z) == 0
+	// fmt.Println(result2)
+
+}
