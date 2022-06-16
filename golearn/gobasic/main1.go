56f2563c1c0741b1f7d8a009e04e2ebd8fa14b97
vinaycharlie01 <106230026+vinaycharlie01@users.noreply.github.com>
2022-06-16 14:26:44 +0530

    Create main1.go

---
+++ golearn/gobasic/main1.go
@@ -0,0 +1,12 @@
+package main
+
+func main() {
+	//Coding Exercise #2
+	//1.Declare a, b, c, d using a single var keyword (multiple variable declaration) for better readability.
+	var a, b, c, d = 10, 10.2, "vijay", true
+	//2. Declare x, y and z on a single line -> multiple short declarations
+	x, y, z := 10, 29, "vufn"
+	//Change the program to run without error using the blank identifier (_)
+	_, _, _, _, _, _, _ = a, b, c, d, x, y, z
+
+}
